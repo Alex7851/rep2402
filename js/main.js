ec25187e1b5eeb1e246478dc55d876885fd003f4
MyName <emailgre1232@fdfsd.ru>
2019-02-24 19:01:12 +0300

    Эксперимент с проверками под смену валюты

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-$('.wrapper .leftblock').on('input', "input:text", {typeExchange: "sell" }, mainEngine)
+$('.wrapper .leftblock').on('input', "input:text", {typeExchange: "sell", checkStatus: "present" }, mainEngine)
 $('.wrapper .rightblock').on('input', "input:text", {typeExchange: "buy" }, mainEngine)
+$('.wrapper [name="firstSelector"]').on('input', {typeExchange: "sell", checkStatus: "none"}, mainEngine)
 
 
 function mainEngine(e){
@@ -10,24 +11,22 @@ function mainEngine(e){
 	var secondInputField;
 	var fieldNameToCheck;
 
-if (e.data.typeExchange=="sell") {
-	selector_='[name="firstSelector"]';
-	firstInputField='[name="inputField1"]';
-	secondInputField='[name="inputField2"]';
-	firstSpanField='.dataSellWmr';
- 	secondSpanField='.dataSellWmz';
- 	fieldNameToCheck="inputField1";
-}
-else {
-	selector_='[name="secondSelector"]';
-	firstInputField='[name="inputField4"]';
-	secondInputField='[name="inputField3"]';
-	firstSpanField='.dataBuyWmr';
- 	secondSpanField='.dataBuyWmz';
- 	fieldNameToCheck="inputField4";
-
-
-}
+	if (e.data.typeExchange=="sell") {
+		selector_='[name="firstSelector"]';
+		firstInputField='[name="inputField1"]';
+		secondInputField='[name="inputField2"]';
+		firstSpanField='.dataSellWmr';
+	 	secondSpanField='.dataSellWmz';
+	 	fieldNameToCheck="inputField1";
+	}
+	else {
+		selector_='[name="secondSelector"]';
+		firstInputField='[name="inputField4"]';
+		secondInputField='[name="inputField3"]';
+		firstSpanField='.dataBuyWmr';
+	 	secondSpanField='.dataBuyWmz';
+	 	fieldNameToCheck="inputField4";
+	}
 
 
 var selectorStatus=parseInt($(selector_).children("option:selected").val());
@@ -35,19 +34,19 @@ var fieldName=$(this).attr('name');
 var currency;
 
 
-if ($(this).val() == '') {
+if ($(this).val() == '' && e.data.checkStatus=="present") {
 	$(firstInputField).val('');
 	$(secondInputField).val('');
 		return;
 }
 
-if (!selectorStatus) {
+if (!selectorStatus && e.data.checkStatus=="present") {
 		$(firstInputField).val("Выберите валюту");
 		$(secondInputField).val("Выберите валюту");
 		return;
 }
 
-if (!$.isNumeric($(this).val())) {
+if (!$.isNumeric($(this).val()) && e.data.checkStatus=="present") {
 	$(firstInputField).val("");
 	$(secondInputField).val("");
 	return;
@@ -65,6 +64,21 @@ if (!$.isNumeric($(this).val())) {
 	}
 	
 	var result;
+
+
+	if (e.data.checkStatus=="none") {
+
+	if ($(this).attr('name')=="firstSelector"){
+		result=(parseInt($(firstInputField).val())*currency).toFixed(2)
+		$(secondInputField).val(result)
+	};
+	
+
+	
+
+
+	return;
+	}
 
 	if (fieldName==fieldNameToCheck) {
 
