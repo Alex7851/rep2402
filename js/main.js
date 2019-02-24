bd7cb674d6c8f15899f6880cadd6dbfb1837e42b
MyName <emailgre1232@fdfsd.ru>
2019-02-24 18:11:48 +0300

    Настроил на оба блока без смена валюты

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $('.wrapper .leftblock').on('input', "input:text", {typeExchange: "sell" }, mainEngine)
+$('.wrapper .rightblock').on('input', "input:text", {typeExchange: "buy" }, mainEngine)
 
 
 function mainEngine(e){
-var selector_;
-var firstInputField;
-var secondInputField;
-var firstInputField;
-var secondInputField;
+	var selector_;
+	var firstInputField;
+	var secondInputField;
+	var firstInputField;
+	var secondInputField;
+	var fieldNameToCheck;
 
 if (e.data.typeExchange=="sell") {
 	selector_='[name="firstSelector"]';
@@ -14,13 +16,25 @@ if (e.data.typeExchange=="sell") {
 	secondInputField='[name="inputField2"]';
 	firstSpanField='.dataSellWmr';
  	secondSpanField='.dataSellWmz';
+ 	fieldNameToCheck="inputField1";
+}
+else {
+	selector_='[name="secondSelector"]';
+	firstInputField='[name="inputField4"]';
+	secondInputField='[name="inputField3"]';
+	firstSpanField='.dataBuyWmr';
+ 	secondSpanField='.dataBuyWmz';
+ 	fieldNameToCheck="inputField4";
+
+
 }
 
 
 var selectorStatus=parseInt($(selector_).children("option:selected").val());
 var fieldName=$(this).attr('name');
 var currency;
-console.log(selectorStatus);
+
+
 if ($(this).val() == '') {
 	$(firstInputField).val('');
 	$(secondInputField).val('');
@@ -28,47 +42,31 @@ if ($(this).val() == '') {
 }
 
 if (!selectorStatus) {
-
 		$(firstInputField).val("Выберите валюту");
 		$(secondInputField).val("Выберите валюту");
 		return;
-		
-
 }
 
 if (!$.isNumeric($(this).val())) {
-$(firstInputField).val("");
-$(secondInputField).val("");
-
-
-
-return;
-
-
+	$(firstInputField).val("");
+	$(secondInputField).val("");
+	return;
 }
 	
 	
 	switch(selectorStatus)	{
-
-		
-
 		case 1:
 		currency= parseFloat($(firstSpanField).text());
 		break;
-
+		
 		case 2:
 		currency= parseFloat($(secondSpanField).text());
 		break;
-
 	}
-	// console.log(selectorStatus)
-	// console.log($('.dataSellWmr').text())
-	// console.log(currency)
-
-
+	
 	var result;
 
-	if (fieldName=="inputField1") {
+	if (fieldName==fieldNameToCheck) {
 
 	   	result=(parseInt($(this).val())*currency).toFixed(2)
 		$(secondInputField).val(result)
@@ -78,7 +76,4 @@ return;
 		result=(parseInt($(this).val())/currency).toFixed(2)
 		$(firstInputField).val(result)
 	}
-
-
-
 }
